2a35f21477debf693cf187744e196b9c72e34762
Nikitha-79 <nikithamaryada@gmail.com>
2020-09-28 03:05:49 -0700

    Created a FootBallController

---
+++ BasketBall/Controllers/FootBallController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasketBall.Controllers
+{
+    public class FootBallController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
